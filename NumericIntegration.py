2ac8cc393ab2ea29d012554183523c462dd7c2d7
YanYu Yee <yeey@flip1.engr.oregonstate.edu>
2014-04-18 15:29:30 -0700

    Fixed incorrect directory for script

--- NumericIntegration.py
+++ NumericIntegration.py
@@ -1,11 +1,12 @@
 import os
+import math
 from subprocess import Popen, PIPE
 
 START_NUM_THREAD = 1
 END_NUM_THREAD = 9
 
-START_SUBDIVISIONS = 100
-END_SUBDIVISIONS = 40000
+BASE = 2
+UPPERBOUND = 19
 
 def main():
 	
@@ -13,11 +14,11 @@ def main():
 	print "num_thread,num_divisions,time,total_volume"
 
 	for thread_num in range(START_NUM_THREAD, END_NUM_THREAD):
-		for subdivision_num in range(START_SUBDIVISIONS, END_SUBDIVISIONS, 500):
-			process = Popen(["./build/NumericalIntegration", "-D", str(subdivision_num), "-T", str(thread_num)], stdout = PIPE)
+		for subdivision_num in range(0, UPPERBOUND):
+			process = Popen(["./build/NumericIntegration", "-D", str(math.pow(2,subdivision_num)), "-T", str(thread_num)], stdout = PIPE)
 			(output, err) = process.communicate()
 			process.wait()
-			print str(thread_num) + "," + str(subdivision_num) + "," + output.rstrip()
+			print str(thread_num) + "," + str(math.pow(2,subdivision_num)) + "," + output.rstrip()
 	return
 
 if __name__ == "__main__":
